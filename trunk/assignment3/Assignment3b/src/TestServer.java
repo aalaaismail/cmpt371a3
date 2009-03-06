b3050222371f8382863b0724234b81696c95d0d0
b.man808 <b.man808@8ca94c5c-03b6-11de-85c2-bb0660c75f22>
2009-03-06 18:56:00 +0000

    my crappy code

--- trunk/assignment3/Assignment3b/src/TestServer.java
+++ trunk/assignment3/Assignment3b/src/TestServer.java
@@ -27,7 +27,7 @@ public class TestServer {
 	     int dst_port = Integer.parseInt(args[1]);
 	     int local_port = Integer.parseInt(args[2]);
 	     	      
-	     RDT rdt = new RDT(hostname, dst_port, local_port, 5, 5, 2);
+	     RDT rdt = new RDT(hostname, dst_port, local_port, 5, 5, 1);
 	     RDT.setLossRate(0.2);
 	     byte[] buf = new byte[RDT.MSS];  	     
 	     System.out.println("\n\n ======>SERVER IS WAITING TO RECEIVE<========\n\n " );
@@ -35,11 +35,17 @@ public class TestServer {
 	     
 	     while (true) {
 	    	 int size = rdt.receive(buf, RDT.MSS);
+	    	 if(size == -1){
+	    		 System.out.println("SIZE = -1");
+	    		 break;
+	    	 }
 	    	// System.out.println("size is " + size);
 	    	 for (int i=0; i<size; i++)
 	    		 System.out.print(buf[i]);
 	    	 System.out.println(" ");
 	    	 System.out.flush();
 	     } 
+	     rdt.close();
+	     System.out.println("Server done");
 	}
 }
